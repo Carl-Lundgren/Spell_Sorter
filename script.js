8b2935fcf4cc95dfeea0a8c41aac2d9acfe9ec93
Carl <lundgren.e.carl@gmail.com>
2021-04-26 16:48:39 -0700

    added js constants

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const submit = document.querySelector('.spell-search-form');
     submit.addEventListener('submit', (e) => {
         e.preventDefault();
-
+        const li = document.createElement('li');
+        const spellList = document.querySelector('#spell-list')
+        
         fetch (`https://www.dnd5eapi.co/api/spells/${}`, )
             .then(response => response.json())
-            .then(data => data))
+            .then(data => data)
             .catch(error => {
                 document.body.append(error.message);
             });
