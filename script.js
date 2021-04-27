1dc4a16933f54e6b0f632b6c516b98f4a423694b
Carl <lundgren.e.carl@gmail.com>
2021-04-27 14:00:59 -0700

    completed initial searcher

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
         
         const spellList = document.querySelector('#spell-list');
         const input = document.querySelector('#spell-searched');
+        spellList.innerHTML = '';
 
         fetch (`https://www.dnd5eapi.co/api/spells/?name=${input.value}`)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                for (const key in data) {
+                console.log(data.results);
+                for (const key in data.results) {
+                    console.log(data.results[key]);
                     const li = document.createElement('li');
-                    li.innerText = `${data.name}`
+                    li.innerText = `${data.results[key].name}`;
                     spellList.append(li);
                 }
             })
